607379de7a5933dd5f006fcb8b019886ad8f8fb6
Anthony <anthonytrading81@gmail.com>
2013-09-26 22:49:49 +0800

    Update StripeHelper class with some convenience methods

--- src/AnthonyVipond/StripeHelper/StripeHelper.php
+++ src/AnthonyVipond/StripeHelper/StripeHelper.php
@@ -1,20 +1,19 @@
 <?php namespace AnthonyVipond\StripeHelper;
 
 use \Config;
- 
+
 class StripeHelper {
 
-	public static $api_key;
+	public static function api_key() {
+		return Config::get('stripehelper::api_key');
+	}
 
-	public static $pub_key;
+	public static function pub_key() {
+		return Config::get('stripehelper::publishable_key');
+	}
 
-	public static $currency;
-
-	public function __construct() {
-
-		static::$api_key  = Config::get('stripe.api_key');
-		static::$pub_key  = Config::get('stripe.publishable_key');
-		static::$currency = Config::get('stripe.currency');
+	public static function currency() {
+		return Config::get('stripehelper::currency');
 	}
  
 }
