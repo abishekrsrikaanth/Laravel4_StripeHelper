a53037449fd75381d44cbd6b5ced4326f617cfda
Anthony <anthonytrading81@gmail.com>
2013-09-27 01:26:31 +0800

    Fix error in migration table

--- src/migrations/2013_09_25_033455_create_payment_errors_table.php
+++ src/migrations/2013_09_25_033455_create_payment_errors_table.php
@@ -14,17 +14,14 @@ class CreatePaymentErrorsTable extends Migration {
 		if (!Schema::hasTable('payment_errors')) {	
 			Schema::create('payment_errors', function($table)
 			{
-			    Schema::create('payment_errors', function($table)
-				{
-					$table->increments('id');
-					$table->string('email')->length(255);
-					$table->string('type')->length(255);
-					$table->text('code');
-					$table->text('param');
-					$table->text('message');
-					$table->date('date');
-				});
-			}
+				$table->increments('id');
+				$table->string('email')->length(255);
+				$table->string('type')->length(255);
+				$table->text('code');
+				$table->text('param');
+				$table->text('message');
+				$table->date('date');
+			});
 		} else {
 			throw new Exception('You already have a "payment_errors" table. Delete this table, or temporarily rename it, then install this package again');
 		}
